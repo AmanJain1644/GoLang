bc81186a30915d3a499975d9ca7ce9e2b7e3a245
amanjain1644 <amanjain1644@gmail.com>
2025-01-30 00:44:41 +0530

    learnt about channels

--- Package/main.go
+++ Package/main.go
@@ -6,10 +6,25 @@ import (
 	"github.com/amanjain1644/GoLang/Package/helpers"
 )
 
+func GetTheNum(intChan chan int){
+	intChan <- helpers.RandomNumber(10);
+
+}
+
 func main(){
 	var myVar helpers.SomeType
 	myVar.TypeName = "SomeName"
 	myVar.TypeNumber = 99
 
 	log.Println(myVar.TypeName,myVar.TypeNumber)
+
+	//channels 
+	intChan := make(chan int);
+	defer close(intChan)
+
+	go GetTheNum(intChan)
+
+	num:= <-intChan
+	log.Println(num)
+
 }
