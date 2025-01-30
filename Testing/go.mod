b248a1a959c5e35c6758fed16ade2d50e205c9d8
amanjain1644 <amanjain1644@gmail.com>
2025-01-31 00:13:53 +0530

    testing code go fundametals completed here

---
+++ Testing/go.mod
@@ -0,0 +1,3 @@
+module www.github.com/AmanJain1644/GoLang/tree/master/Testing
+
+go 1.23.2
--- Testing/main_test.go
+++ Testing/main_test.go
@@ -1,11 +1,43 @@
 package main
 
 import (
-	"log"
+	
 	"testing"
 )
 
 // this is the manual manner to do test
+var tests = []struct{
+	name string
+	divident float32
+	divisor float32
+	expected float32
+	isErr bool
+}{
+	{"valid-data",100.0,10.0,10.0,false},
+	{"Invalid-data",100.0,0.0,0.0,true},
+	{"valid-data2",100.0,5.0,20.0,false},
+
+
+}
+
+func TestDivison(t *testing.T){
+	for _,tt:= range tests{
+		got,err:=divide(tt.divident,tt.divisor)
+		if tt.isErr{
+			if err==nil {
+				t.Error("expected an error but did not get one")
+			}
+		}else{
+			if err!=nil{
+				t.Error("got unexpected error", err.Error())
+			}
+		}
+
+		if got != tt.expected{
+			t.Errorf("expected %f but got %f , but %f, %f",tt.expected,got,tt.divident,tt.divisor)
+		}
+	}
+}
 
 func TestDivide(t *testing.T){
 	_,err := divide(100.0,10.0)
