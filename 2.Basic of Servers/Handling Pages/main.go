62a5fc1d08160b22dbb1656fa783187dc1950c27
amanjain1644 <amanjain1644@gmail.com>
2025-02-06 23:08:15 +0530

    handled error in the code

--- 2.Basic of Servers/Handling Pages/main.go
+++ 2.Basic of Servers/Handling Pages/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -19,9 +20,26 @@ func addValues(x,y int)int{
 	return x+y
 }
 
+func Divide(w http.ResponseWriter,r *http.Request){
+	n,err:=divide(100.0,0.0)
+	if err!=nil{
+		fmt.Fprintf(w,"Error %s",err.Error())
+		return
+	}
+	fmt.Fprintf(w,"on dividing %f with %f we get %f",100.0,0.0,n)
+}
+
+func divide(x,y float32)(float32,error){
+	if y==0{
+		return 0,errors.New("can not divide with zero")
+	}
+	return x/y,nil
+}
+
 func main() {
 	http.HandleFunc("/",Home)
 	http.HandleFunc("/about",About)
+	http.HandleFunc("/divide",Divide)
 
 	fmt.Println(fmt.Sprintf("Started listening on port %s",portNumber))
 	_ = http.ListenAndServe(portNumber,nil)
